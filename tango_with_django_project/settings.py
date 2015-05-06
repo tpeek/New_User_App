d538b135270070df3d28679ff5c9594263acc002
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-05-06 13:32:10 -0800

    tttt

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -140,4 +140,22 @@ STATICFILES_DIRS = (
 #    STATIC_PATH,
 #)
 
+
+
+import os
+import psycopg2
+import urlparse
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
